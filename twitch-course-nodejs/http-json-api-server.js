00596ecb1dee9d53fc64f2c50be221692f79aa01
R4mmst3in <pjmarpe@gmail.com>
2026-06-28 21:52:09 +0200

    Final

--- twitch-course-nodejs/http-json-api-server.js
+++ twitch-course-nodejs/http-json-api-server.js
@@ -1,14 +1,39 @@
 const http = require('node:http')
+const port = process.argv[2]
 
-port = process.argv[2]
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-
-  if (req.method === 'GET') {
-    
+function parseTime(time) {
+  return {
+    'hour'   : time.getHours(),
+    'minute' : time.getMinutes(),
+    'second' : time.getSeconds()
   }
 
+}
+
+function unixTime(time) {
+  return {
+    unixtime: time.getTime()
+  }
+}
+
+const routes = {
+  '/api/parsetime' : (parsedUrl) => {
+    const time = new Date(parsedUrl.searchParams.get('iso'))
+    return parseTime(time)
+  },
+  '/api/unixtime' : (parsedUrl) => {
+    const time = new Date(parsedUrl.searchParams.get('iso'))
+    return unixTime(time)
+  }
+}
+
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(req.url, `http://localhost:${port}`)
+  const route = routes[parsedUrl.pathname]
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(route(parsedUrl)))
 
 })
 
--- twitch-course-nodejs/http-uppercaserer.js
+++ twitch-course-nodejs/http-uppercaserer.js
@@ -7,7 +7,7 @@ const map = require('through2-map')
 //        return chunk.toString().split('').reverse().join('')
 //      })).pipe(outStream)
 
-port = process.argv[2]
+const port = process.argv[2]
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'content-type':'text/plain'})
